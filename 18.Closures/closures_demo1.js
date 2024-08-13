ff54ea9b9a86c92aa510de206ddad41e5532f11b
Aman Singh <amansinghbhumihar2510@gmail.com>
2024-08-13 09:04:15 +0530

    Made changes in closures

--- 18.Closures/closures_demo1.js
+++ 18.Closures/closures_demo1.js
@@ -8,6 +8,8 @@ console.log("starting");
 todo("assignments");
 console.log("ending");
 
+
+
 /** To clarify that function todo is over when function fun is called */
 function todo(task) {
     console.log("start of todo");
@@ -19,4 +21,21 @@ function todo(task) {
 
 console.log("starting");
 todo("assignments");
-console.log("ending");   
+console.log("ending");
+
+
+
+/** When task is reassigned then the fun function task is also changed */
+function todo() {
+    var task = "assignments";
+    console.log("start of todo");
+    setTimeout(function fun() {
+        console.log("completed", task);
+    }, 2000);
+    task = "dayananda";
+    console.log("end of todo");
+}
+
+console.log("starting");
+todo();
+console.log("ending");  
--- 18.Closures/tempCodeRunnerFile.js
+++ 18.Closures/tempCodeRunnerFile.js
@@ -1,11 +1,13 @@
-function todo(task) {
+function todo() {
+    var task = "assignments";
     console.log("start of todo");
     setTimeout(function fun() {
         console.log("completed", task);
     }, 2000);
+    task = "dayananda";
     console.log("end of todo");
 }
 
 console.log("starting");
-todo("assignments");
-console.log("ending"); 
+todo();
+console.log("ending");
